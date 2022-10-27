9e28c586d5a1d3fdd8835887b12a2e04766a4004
DanielParraE <daniel.parra216710@potros.itson.edu.mx>
2022-10-26 20:20:46 -0700

    Proyecto Vacio

---
+++ src/proyectofinalagiles/ProyectoFinalAgiles.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectofinalagiles;
+
+/**
+ *
+ * @author danie
+ */
+public class ProyectoFinalAgiles {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
